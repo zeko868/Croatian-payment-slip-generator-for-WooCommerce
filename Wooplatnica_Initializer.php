df7a59618586026913e9eb1cf9c4cd8d022a8941
Petar Šestak <zeko868@hotmail.com>
2021-10-07 00:18:59 +0200

    Register uninstall hook to remove plugin data and settings
    Plugin data refers to e-mail attachment files that are stored in temporary directory when e-mails are not sent using PHPMailer module.
    Plugin settings are only removed from the database when the WC_REMOVE_ALL_DATA option is defined in wp-config file.

--- Wooplatnica_Initializer.php
+++ Wooplatnica_Initializer.php
@@ -20,11 +20,10 @@ class Wooplatnica_Initializer {
     /**
      * @var string
      */
-    protected $plugin_id;
+    static $plugin_id = 'wooplatnica-croatia';
 
 
     public function __construct() {
-        $this->plugin_id = 'wooplatnica-croatia';
         add_action('plugins_loaded', [$this, 'init'], 0);
     }
 
@@ -40,6 +39,7 @@ class Wooplatnica_Initializer {
 
         add_action( 'woocommerce_loaded', array( $this, 'load_plugin' ) );
         add_filter( 'plugin_action_links', array( $this, 'add_settings_link' ), 10, 2 );
+        register_uninstall_hook(__FILE__, array( get_class($this), 'delete_plugin_data' ) );
 
         foreach ($module_classes as $module_class => $init) {
             require path_join(dirname(__FILE__), "src/$module_class.php");
@@ -51,8 +51,40 @@ class Wooplatnica_Initializer {
 
     function add_settings_link($links, $file) {
         if ($file === plugin_basename(__FILE__)) {
-            $links[] = '<a href="' . get_admin_url( null, 'admin.php?page=wc-settings&tab=checkout&section=' . $this->plugin_id ) . '">' . __( 'Settings' ) . '</a>';
+            $links[] = '<a href="' . get_admin_url( null, 'admin.php?page=wc-settings&tab=checkout&section=' . self::$plugin_id ) . '">' . __( 'Settings' ) . '</a>';
         }
         return $links;
+    }
+
+    static function delete_plugin_data() {
+        $plugin_id = self::$plugin_id;
+        $stored_payment_slips_tmp_directory = path_join(sys_get_temp_dir(), "$plugin_id-data");
+        if (file_exists($stored_payment_slips_tmp_directory)) {
+            self::delete_recursively($stored_payment_slips_tmp_directory);
+        }
+        
+        /*
+         * Only remove ALL plugin settings data if WC_REMOVE_ALL_DATA constant is set to true in user's
+         * wp-config.php. This is to prevent data loss when deleting the plugin from the backend
+         * and to ensure only the site owner can perform this action.
+         */
+        if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
+            // Delete options.
+            delete_option( "woocommerce_{$plugin_id}_settings" );
+        }
+    }
+
+    private static function delete_recursively($dir_path) {
+        $file_iterator = new FilesystemIterator($dir_path);
+        foreach ($file_iterator as $file_info) {
+            $file_path = $file_info->getPathname();
+            if (is_dir($file_path)) {
+                self::delete_recursively($file_path);
+            }
+            else {
+                unlink($file_path);
+            }
+        }
+        return rmdir($dir_path);
     }
 }
